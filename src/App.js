d2941238343483db33d0f65cbe6a37fb66035ee3
akshay3098829 <akshay.mohan@experionglobal.com>
2024-01-23 14:51:41 +0530

    git add .git add .git add .git add .!

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Amplify and React Project
+          Welcome to Amplify and React Project !!!!!!!!!!!!!!
         </p>
         <a
           className="App-link"
